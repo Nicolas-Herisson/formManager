76563af02057a159d81c6378b7f9804aa6db259a
Nicolas-Herisson <nicolas.herisson77@gmail.com>
2025-09-21 22:48:44 +0200

    chore: add isPublieshed on response route

--- backend/routers/response.router.ts
+++ backend/routers/response.router.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import * as responseController from "../controllers/response.controller.js";
 import { sanitizer } from "../middlewares/sanitizer.mw.js";
+import isPublished from "../middlewares/is_published.mw.js";
 
 const responseRouter = Router();
 
-responseRouter.post("/:form_id/responses", sanitizer, responseController.createResponse);
+responseRouter.post("/:form_id/responses", sanitizer, isPublished, responseController.createResponse);
 responseRouter.get("/:form_id/responses", responseController.getResponses);
 responseRouter.get("/:form_id/responses/:response_id", responseController.getResponse);
 responseRouter.delete("/:form_id/responses/:response_id", responseController.deleteResponse);
 responseRouter.delete("/:form_id/responses", responseController.deleteResponsesByFormId);
-responseRouter.put("/:form_id/responses/:response_id", sanitizer, responseController.updateResponse);
+responseRouter.put("/:form_id/responses/:response_id", sanitizer, isPublished, responseController.updateResponse);
 
 export default responseRouter;
